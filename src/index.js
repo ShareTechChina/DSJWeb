8cf881bdfeea87eafb6c54f731c0b9c09266447a
xiewang <xiewang0501@126.com>
2017-01-24 21:09:00 +0800

    fix f7

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import Home from './pages/home';
 import './index.css';
 
-window.f7 = new window.Framework7();
+window.f7 = new window.Framework7({
+    pushState: true,
+});
+
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+    <Home />,
+    document.getElementById('root')
 );
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+    componentDidMount() {
+    }
+
     render() {
         return (
             <div className="App">
@@ -15,26 +19,20 @@ class App extends Component {
                     <div className="view view-main">
                         <div className="navbar">
                             <div className="navbar-inner">
-                                <div className="center sliding">Awesome App</div>
+                                <div className="left"></div>
+                                <div className="center sliding">笔记详情</div>
                                 <div className="right">
-                                    <a href="#" className="link icon-only open-panel"><i className="icon icon-bars-blue"></i></a>
                                 </div>
                             </div>
                         </div>
                         <div className="pages navbar-through toolbar-through">
                             <div data-page="index" className="page">
                                 <div className="page-content">
-                                    <p>Page content goes here</p>
-                                    <a href="about.html">About app</a>
+
                                 </div>
                             </div>
                         </div>
-                        <div className="toolbar">
-                            <div className="toolbar-inner">
-                                <a href="#" className="link">Link 1</a>
-                                <a href="#" className="link">Link 2</a>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
             </div>
