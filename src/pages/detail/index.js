87078db6c1d74034f8615c9d799ab64c9abf00d6
xiewang <xiewang0501@126.com>
2017-01-30 22:19:29 +0800

    detail page improve

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import {fetchDetail} from '../../actions/detail';
 import { connect } from 'react-redux';
+import { timeFormat } from '../../utils/common';
 
 class Detail extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
     componentDidMount() {
-        let mySwiper = window.f7.swiper('.swiper-container', {
-            speed: 400,
-            spaceBetween: 100,
-            pagination:'.swiper-pagination',
-            spaceBetween: 0
-        });
-
         const { dispatch } = this.props;
         dispatch(fetchDetail(1));
     }
 
+    componentDidUpdate() {
+        window.f7.swiper('.swiper-container', {
+            speed: 400,
+            pagination: '.swiper-pagination',
+            spaceBetween: 0
+        });
+    }
+
     render() {
+        let { detail } = this.props;
+        let noteId = 1;
+        let slide;
+        if (detail.note[noteId]) {
+            slide = detail.note[noteId].images.map((val, key) => {
+                return ( <div className="swiper-slide" key={key}>
+                    <img src={val.image.url}/>
+                </div>);
+            }, this);
+        } else {
+            slide =
+                <div className="swiper-slide">
+                    <img src=''/>
+                </div>
+        }
         return (
             <div className="Detail">
                 <div className="statusbar-overlay"></div>
@@ -34,7 +51,7 @@ class Detail extends Component {
                         <div className="navbar">
                             <div className="navbar-inner">
                                 <div className="left"></div>
-                                <div className="center sliding">笔记详情</div>
+                                <div className="center sliding">剁手记</div>
                                 <div className="right">
                                 </div>
                             </div>
@@ -44,19 +61,27 @@ class Detail extends Component {
                                 <div className="page-content">
                                     <div className="user">
                                         <div className="portrait">
+                                            <img
+                                                src={detail.note[noteId] ? detail.note[noteId].portrait : 'https://avatars2.githubusercontent.com/u/19884155?v=3&s=200'}
+                                                alt=""/>
+                                        </div>
+                                        <div className="nick">
+                                            <div className="nick-name">{detail.note[noteId] ? detail.note[noteId].nickname : '' }</div>
+                                            <div className="publish-time">{detail.note[noteId] ? timeFormat(detail.note[noteId].publishTime, 'yyyy年MM月dd日 hh:mm:ss') : ''}</div>
                                         </div>
                                     </div>
                                     <div className="swiper-container">
                                         <div className="swiper-wrapper">
-                                            <div className="swiper-slide">Slide 1</div>
-                                            <div className="swiper-slide">Slide 2</div>
-                                            <div className="swiper-slide">Slide 3</div>
+                                            {
+                                                slide
+                                            }
                                         </div>
                                         <div className="swiper-pagination"></div>
                                     </div>
                                     <div className="description">
-                                        <p classNme="title">This is title</p>
-                                        <p classNme="content">blablas.........</p>
+                                        <p className="title">{detail.note[noteId] ? detail.note[noteId].title : ''}</p>
+
+                                        <p className="content">{detail.note[noteId] ? detail.note[noteId].content : '' }</p>
                                     </div>
                                     <div className="comment">
                                     </div>
