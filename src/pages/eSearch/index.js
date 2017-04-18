cf0a97d17b94b6306b3ebe7f01de460d77b81e20
xiewang <xiewang0501@126.com>
2017-04-18 23:27:06 +0800

    add icons for eSearch page

--- src/pages/eSearch/index.js
+++ src/pages/eSearch/index.js
@@ -14,10 +14,27 @@ class ESearch extends Component {
 
     }
 
+    _goTo(site) {
+        if(site === 'taobao')
+            location.href = 'https://s.m.taobao.com/h5entry';
+        if(site === 'tmall')
+            location.href = 'https://www.tmall.com/?from=m';
+        if(site === 'jd')
+            location.href = 'https://m.jd.com/';
+        if(site === 'baidu')
+            location.href = 'https://m.baidu.com/';
+    }
+
     render() {
         return (
             <div className="eSearch">
-
+                <div className="title">搜索快捷入口</div>
+                <div className="icons">
+                    <div className="icon" id="taobao" onClick={()=>this._goTo('taobao')}></div>
+                    <div className="icon" id="tmall" onClick={()=>this._goTo('tmall')}></div>
+                    <div className="icon" id="jd" onClick={()=>this._goTo('jd')}></div>
+                    <div className="icon" id="baidu" onClick={()=>this._goTo('baidu')}></div>
+                </div>
             </div>
         );
     }
